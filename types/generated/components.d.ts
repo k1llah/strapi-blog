bdd95db68d0f732e8e51462d5cbe83c61fff8e08
John Doe <surenbound@yandex.ru>
2024-04-18 01:34:31 +0300

    init strapi

---
+++ types/generated/components.d.ts
@@ -0,0 +1,58 @@
+import type { Schema, Attribute } from '@strapi/strapi';
+
+export interface ContentContent extends Schema.Component {
+  collectionName: 'components_content_contents';
+  info: {
+    displayName: 'content';
+    icon: 'medium';
+  };
+  attributes: {
+    content: Attribute.RichText & Attribute.Required;
+  };
+}
+
+export interface CoupleImagesCoupleImages extends Schema.Component {
+  collectionName: 'components_couple_images_couple_images';
+  info: {
+    displayName: 'couple-images';
+    icon: 'stack';
+  };
+  attributes: {
+    content: Attribute.RichText & Attribute.Required;
+  };
+}
+
+export interface SingleImageSingleImage extends Schema.Component {
+  collectionName: 'components_single_image_single_images';
+  info: {
+    displayName: 'single-image';
+    icon: 'attachment';
+  };
+  attributes: {};
+}
+
+export interface TitleTitle extends Schema.Component {
+  collectionName: 'components_title_titles';
+  info: {
+    displayName: 'title';
+    icon: 'strikeThrough';
+  };
+  attributes: {
+    title: Attribute.String &
+      Attribute.Required &
+      Attribute.SetMinMaxLength<{
+        minLength: 2;
+      }>;
+  };
+}
+
+declare module '@strapi/types' {
+  export module Shared {
+    export interface Components {
+      'content.content': ContentContent;
+      'couple-images.couple-images': CoupleImagesCoupleImages;
+      'single-image.single-image': SingleImageSingleImage;
+      'title.title': TitleTitle;
+    }
+  }
+}
